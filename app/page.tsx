e734ca6decfa69deba5e7d0a1d97b830d326153a
almoussawiiiiq8-oss <almoussawiiiiq8@gmail.com>
2026-02-09 05:09:26 +0300

    Create page.tsx
    add home page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>بطولاتي ✅</h1>
+      <p>الموقع شغال على Vercel</p>
+    </main>
+  );
+}
